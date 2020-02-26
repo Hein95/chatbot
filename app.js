8fb5e15c061f312c43b46d66273d8158e204f056
Akshay Kashyap <kashyapa@union.edu>
2020-02-26 10:28:50 +0630

    send message back

--- app.js
+++ app.js
@@ -74,6 +74,11 @@ function processPostback(event) {
       sendMessage(senderId, {text: message});
     });
   }
+  else if (payload === "Correct") {
+    sendMessage(senderId, {text: "Awesome! What would you like to find out? Enter 'plot', 'date', 'runtime', 'director', 'cast' or 'rating' for the various details."});
+  } else if (payload === "Incorrect") {
+    sendMessage(senderId, {text: "Oops! Sorry about that. Try using the exact title of the movie"});
+  }
 }
 
 // sends message to user
@@ -126,4 +131,71 @@ function processMessage(event) {
       sendMessage(senderId, {text: "Sorry, I don't understand your request."});
     }
   }
+}
+//find message
+function getMovieDetail(userId, field) {
+  Movie.findOne({user_id: userId}, function(err, movie) {
+    if(err) {
+      sendMessage(userId, {text: "Something went wrong. Try again"});
+    } else {
+      sendMessage(userId, {text: movie[field]});
+    }
+  });
+}
+
+function findMovie(userId, movieTitle) {
+  request("http://www.omdbapi.com/?type=movie&amp;t=" + movieTitle, function (error, response, body) {
+    if (!error &amp;&amp; response.statusCode === 200) {
+      var movieObj = JSON.parse(body);
+      if (movieObj.Response === "True") {
+        var query = {user_id: userId};
+        var update = {
+          user_id: userId,
+          title: movieObj.Title,
+          plot: movieObj.Plot,
+          date: movieObj.Released,
+          runtime: movieObj.Runtime,
+          director: movieObj.Director,
+          cast: movieObj.Actors,
+          rating: movieObj.imdbRating,
+          poster_url:movieObj.Poster
+        };
+        var options = {upsert: true};
+        Movie.findOneAndUpdate(query, update, options, function(err, mov) {
+          if (err) {
+            console.log("Database error: " + err);
+          } else {
+            message = {
+              attachment: {
+                type: "template",
+                payload: {
+                  template_type: "generic",
+                  elements: [{
+                    title: movieObj.Title,
+                    subtitle: "Is this the movie you are looking for?",
+                    image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
+                    buttons: [{
+                      type: "postback",
+                      title: "Yes",
+                      payload: "Correct"
+                    }, {
+                      type: "postback",
+                      title: "No",
+                      payload: "Incorrect"
+                    }]
+                  }]
+                }
+              }
+            };
+            sendMessage(userId, message);
+          }
+        });
+      } else {
+          console.log(movieObj.Error);
+          sendMessage(userId, {text: movieObj.Error});
+      }
+    } else {
+      sendMessage(userId, {text: "Something went wrong. Try again."});
+    }
+  });
 }
